c8424d058a476ccba02932dbb4864bb461275513
Moatazahmed156 <moatazahmed696@gmail.com>
2025-09-14 09:48:09 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:5550/api/event/quizGame");
+        const response = await axios.get("https://api.180daraga.com/api/event/quizGame");
         setUsers(response.data.data);
         setLoading(false);
       } catch (err) {
